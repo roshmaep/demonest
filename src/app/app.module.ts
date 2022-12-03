a9c395288ce4102b4b86de55695def6f132c09f7
roshmaep <roshmaep1999@gmail.com>
2022-12-03 12:01:12 +0530

    empreg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { GalleryComponent } from './gallery/gallery.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { EmpregComponent } from './empreg/empreg.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { EmployeeComponent } from './employee/employee.component';
     GalleryComponent,
     AboutComponent,
     ContactComponent,
-    EmployeeComponent
+    EmployeeComponent,
+    EmpregComponent
   ],
   imports: [
     BrowserModule,
