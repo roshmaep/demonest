4f3e92a7fbf0e6eaa189df14876be0190f9555d8
roshmaep <roshmaep1999@gmail.com>
2022-12-03 12:13:44 +0530

    add friend

---
+++ src/app/addfriend/addfriend.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addfriend',
+  templateUrl: './addfriend.component.html',
+  styleUrls: ['./addfriend.component.css']
+})
+export class AddfriendComponent {
+
+}
